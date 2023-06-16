e7f0fcf9b9c63f36233f48c7c7ba4668224f1e61
Daniel <danielkofi4@gmail.com>
2023-06-16 14:51:18 +0000

    set up readline interface

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("YouTube Video Title: \n", (title) => console.log(title));
