4de2444674543067aac0491da7d53f41efcede48
Daniel <danielkofi4@gmail.com>
2023-06-16 15:56:28 +0000

    .gitignore is now working

--- app.js
+++
@@ -1,8 +0,0 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("YouTube Video Title: \n", (title) => console.log(title));
